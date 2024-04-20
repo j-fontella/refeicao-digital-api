2d537d80421240fd7c96e0df14e32cbd5b3c7f81
willian <wyllprograma@gmail.com>
2024-04-20 20:10:17 -0300

    Update

--- src/main/java/digital/refeicao/Debug.java
+++ src/main/java/digital/refeicao/Debug.java
@@ -3,14 +3,6 @@ package digital.refeicao;
 public class Debug {
 
     public static void main(String[] args) {
-        Integer a = 155;
-        Integer b = 155;
-
-        int[] i = new int[3];
-
-
-
-
-        System.out.println(a == b);
+        System.out.println("Hello world");
     }
 }
